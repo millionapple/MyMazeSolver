92e14b91c13456329db251e98a0327d6dd1d4155
GarrettBentley <garrettrussell1999@gmail.com>
2021-05-26 14:59:59 -0500

    First commit

---
+++ src/servlets/GetMazeServlet.java
@@ -0,0 +1,31 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/GetMazeServlet")
+public class GetMazeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public GetMazeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("index.html");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
