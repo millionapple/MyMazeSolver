bdb3bb5c5ce1489eaf979553965240bf69215852
GarrettBentley <garrettrussell1999@gmail.com>
2021-05-27 13:11:38 -0500

    Added logic to the GetMazeServlet for the doGet that will take the width and height from the user and return an array of that size.

--- src/servlets/GetMazeServlet.java
+++ src/servlets/GetMazeServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import beans.Grid;
+
 @WebServlet("/GetMazeServlet")
 public class GetMazeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -16,9 +18,13 @@ public class GetMazeServlet extends HttpServlet {
     }
     //this method is used to get information about the grid the user inputs from the front end
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println(request.getParameter("gridWidth"));
+		Grid grid = new Grid();
+		grid.setGridWidth(Integer.parseInt(request.getParameter("gridWidth")));
+		grid.setGridHeight(Integer.parseInt(request.getParameter("gridHeight")));
+		int[][] gridMap = grid.createGrid();
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.sendRedirect("index.html");
+		response.getWriter().append(gridMap.toString());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
