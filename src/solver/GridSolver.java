d5b569bbeca486fbd423ac24edde9ff49df7e662
GarrettBentley <garrettrussell1999@gmail.com>
2021-06-08 12:34:32 -0500

    Created checkAllNeighbors method that looks to see if the start or end is walled off.

--- src/solver/GridSolver.java
+++ src/solver/GridSolver.java
@@ -7,13 +7,39 @@ public class GridSolver {
 		}
 
 	public boolean gridHasStartAndEnd(int[][] grid) {
+		boolean hasStart = false;
+		boolean hasEnd = false;
 		for(int row = 0; row < grid.length; row++) {
 			for(int column = 0; column < grid[row].length; column++) {
-				if(grid[row][column] == 2 && grid[row][column] == 3) {
-					return true;
+				if(grid[row][column] == 2) {
+					hasStart = true;
+				}else if(grid[row][column] == 3) {
+					hasEnd = true;
 				}
 			}
 		}
-		return false;
+		return hasStart == true && hasEnd == true;
+	}
+	public boolean checkAllNeighbors(int[][] grid) {
+		boolean startPath = true;
+		boolean endPath = true;
+		for(int row = 0; row < grid.length; row++) {
+			for(int column = 0; column < grid[row].length; column++) {
+				if(grid[row][column] == 2) {
+					startPath = isPath(row, column, grid);
+				}else if(grid[row][column] == 3) {
+					endPath = isPath(row, column, grid);
+				}
+			}
+		}
+		return startPath == false || endPath == false ? false : true;
+	}
+	public boolean isPath(int row, int column, int[][] grid) {
+		int numOfWalls = 0;
+		numOfWalls = row - 1 < 0 || grid[row-1][column] == 1 ? numOfWalls+1 : +0;
+		numOfWalls = column - 1 < 0 || grid[row][column-1] == 1 ? numOfWalls+1 : +0;
+		numOfWalls = row +1 >= grid.length || grid[row+1][column] == 1 ? numOfWalls+1 : +0;
+		numOfWalls = column +1 >= grid[row].length || grid[row][column+1] == 1 ?numOfWalls+1 : +0;
+		return numOfWalls == 4 ? false : true;
 	}
 }
--- test/solver/GridSolverTest.java
+++ test/solver/GridSolverTest.java
@@ -56,8 +56,21 @@ public class GridSolverTest {
 		assertEquals(expected, result);
 	}
 	
+	@Test 
+	public void hasBothStartAndEnd() {
+		GridSolver gridSolver = new GridSolver();
+		int[][] grid = new int[4][4];
+		grid[1][1] = 2;
+		grid[2][2] = 3;
+		boolean expected = true;
+		
+		boolean result = gridSolver.gridHasStartAndEnd(grid);
+		
+		assertEquals(expected, result);
+	}
+
 	@Test
- 	public void StartIsSuroundedByWalls() {
+	public void startSurroundedByWalls() {
 		GridSolver gridSolver = new GridSolver();
 		int[][] grid = new int[4][4];
 		for(int row = 0; row < grid.length; row++) {
@@ -66,9 +79,88 @@ public class GridSolverTest {
 			}
 		}
 		grid[1][1] = 2;
-		grid[2][2] =3;
 		boolean expected = false;
 		
+		boolean result = gridSolver.checkAllNeighbors(grid);
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test 
+	public void endSurroundedByWalls() {
+		GridSolver gridSolver = new GridSolver();
+		int[][] grid = new int[4][4];
+		for(int row = 0; row < grid.length; row++) {
+			for(int column = 0; column < grid[row].length; column++) {
+				grid[row][column] = 1;
+			}
+		}
+		grid[1][1] = 3;
+		boolean expected = false;
+		
+		boolean result = gridSolver.checkAllNeighbors(grid);
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void startInCornerSurroundedByWalls() {
+		GridSolver gridSolver = new GridSolver();
+		int[][] grid = new int[4][4];
+		for(int row = 0; row < grid.length; row++) {
+			for(int column = 0; column < grid[row].length; column++) {
+				grid[row][column] = 1;
+			}
+		}
+		grid[0][0] = 2;
+		boolean expected = false;
+		
+		boolean result = gridSolver.checkAllNeighbors(grid);
+		
+		assertEquals(expected, result);
 	}
 
+	@Test
+	public void notSurroundedByWalls() {
+		GridSolver gridSolver = new GridSolver();
+		int[][] grid = new int[4][4];
+		grid[1][1] = 2;
+		grid[2][2] = 3;
+		boolean expected = true;
+		
+		boolean result = gridSolver.checkAllNeighbors(grid);
+		
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void endNotSurroundedStartIs() {
+		GridSolver gridSolver = new GridSolver();
+		int[][] grid = new int[4][4];
+		grid[0][0] = 2;
+		grid[1][0] = 1;
+		grid[0][1] = 1;
+		grid[2][2] = 3;
+		boolean expected = false;
+		
+		boolean result = gridSolver.checkAllNeighbors(grid);
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void startNotSurroundedEndIs() {
+		GridSolver gridSolver = new GridSolver();
+		int[][] grid = new int[4][4];
+		grid[1][1] = 2;
+		grid[3][3] = 3;
+		grid[2][3] = 1;
+		grid[3][2] = 1;
+		boolean expected = false;
+		
+		boolean result = gridSolver.checkAllNeighbors(grid);
+		
+		assertEquals(expected, result);
+	}
 }
+
