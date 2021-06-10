8779e2b7abe36aa7f98f30b09ddb16b2419da844
GarrettBentley <garrettrussell1999@gmail.com>
2021-06-10 14:34:46 -0500

    Making progress on the GridSolver Class. Path can reset to a branch and try a different path until it finds the end. There are still bugs in it.

--- src/solver/GridSolver.java
+++ src/solver/GridSolver.java
@@ -1,5 +1,6 @@
 package solver;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class GridSolver {
@@ -60,12 +61,35 @@ public class GridSolver {
 		return numOfWalls == 4 ? false : true;
 	}
 	
+	ArrayList<int[]> branches = new ArrayList<>();
+	int backToBranchCount = 0;
+	int numOfBranches = 0;
+	
 	public int[][] move(int[][] grid, int[] currentRowAndCol){
 		System.out.println(Arrays.deepToString(grid).replace("], ", "]\n").replace("[[", "\n[").replace("]]", "]"));
+		System.out.println("Branching Paths");
+		for(int[] branch : branches) {
+			System.out.println(Arrays.toString(branch));
+		}
 		int row = currentRowAndCol[0];
 		int column = currentRowAndCol[1];
 		if(grid[row][column] == 5) {
 			grid[row][column] = 4;
+		}
+		if(pathBranches(grid, currentRowAndCol)) {
+			if(branches.isEmpty()) {
+				System.out.println("Is empty and creating a branch");
+				addBranches(currentRowAndCol);
+			}else if(!branches.isEmpty() && !Arrays.equals(currentRowAndCol, branches.get(numOfBranches-1))) {
+				System.out.println("Is not empty and not duplicate, creating a branch");
+				addBranches(currentRowAndCol);
+			}else {
+				System.out.println("not a different branch");
+			}
+			System.out.println("Path Branches");
+			for(int[] branch : branches) {
+				System.out.println(Arrays.toString(branch));
+			}
 		}
 		if(row-1 >= 0 && grid[row-1][column] == 0) {
 			int[] path = new int[]{row-1, column};
@@ -76,7 +100,7 @@ public class GridSolver {
 				move(grid, currentRowAndCol);
 			}else {
 				grid[row-1][column] = 6;
-				move(grid, currentRowAndCol);
+				move(grid, resetToPreviousBranch(branches));
 			}
 		}else if(column+1 < grid[row].length && grid[row][column+1] == 0) {
 			int[] path = new int[]{row, column+1};
@@ -87,7 +111,7 @@ public class GridSolver {
 				move(grid, currentRowAndCol);
 			}else {
 				grid[row][column+1] = 6;
-				move(grid, currentRowAndCol);
+				move(grid, resetToPreviousBranch(branches));
 			}
 		}else if(row+1 < grid.length && grid[row+1][column] == 0) {
 			int[] path = new int[]{row+1, column};
@@ -98,7 +122,7 @@ public class GridSolver {
 				move(grid, currentRowAndCol);
 			}else {
 				grid[row+1][column] = 6;
-				move(grid, currentRowAndCol);
+				move(grid, resetToPreviousBranch(branches));
 			}
 		}else if(column-1 >= 0 && grid[row][column-1] == 0) {
 			int[] path = new int[]{row, column-1};
@@ -109,7 +133,7 @@ public class GridSolver {
 				move(grid, currentRowAndCol);
 			}else {
 				grid[row][column-1] = 6;
-				move(grid, currentRowAndCol);
+				move(grid, resetToPreviousBranch(branches));
 			}
 		}
 		return grid;
@@ -124,5 +148,37 @@ public class GridSolver {
 		numOfWalls = row +1 >= grid.length || grid[row+1][column] == 1 ? numOfWalls+1 : numOfWalls+0;
 		numOfWalls = column +1 >= grid[row].length || grid[row][column+1] == 1 ?numOfWalls+1 : numOfWalls+0;
 		return numOfWalls == 3 ? false : true;
+	}
+	
+	
+	public boolean pathBranches(int[][] grid, int[]currentRowAndCol) {
+		int row = currentRowAndCol[0];
+		int column = currentRowAndCol[1];
+		int paths = 0;
+		paths = row-1 >= 0 && grid[row-1][column] == 0 ? paths+1 : paths;
+		paths = column-1 >= 0 && grid[row][column-1] == 0 ? paths+1 : paths;
+		paths = row+1 < grid.length && grid[row+1][column] == 0 ? paths+1 : paths;
+		paths = column+1 < grid[row].length && grid[row][column+1] == 0 ? paths+1 : paths;
+		System.out.println(paths>1);
+		return paths > 1;
+	}
+	public void addBranches(int[] currentRowAndCol) {
+		int row = currentRowAndCol[0];
+		int column = currentRowAndCol[1];
+		int[] branch = new int[] {row, column};
+		System.out.println("Adding Branch: "+Arrays.toString(branch));
+		branches.add(branch);
+		numOfBranches += 1;
+	}
+	
+	public int[] resetToPreviousBranch(ArrayList<int[]>branches) {
+		System.out.println("To Previous Branch: "+Arrays.toString(branches.get(numOfBranches-1)));
+		int currentBranch = numOfBranches-1;
+		backToBranchCount += 1;
+		System.out.println("backToBranchCount: "+backToBranchCount);
+		int[] previousBranch = new int[2];
+		previousBranch[0] = branches.get(currentBranch)[0];
+		previousBranch[1] = branches.get(currentBranch)[1];
+		return previousBranch;
 	}
 }
